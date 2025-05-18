4e83fffcad738987ab094fd79f33db98fb3e71b6
Julius Truksinas <julius.truksinasinfo@gmail.com>
2025-05-18 16:49:06 +0300

    clean regex in isURL function

--- functions.cpp
+++ functions.cpp
@@ -11,6 +11,6 @@ string cleanWord(const string& word) {
 
 // this function returns true if the word passed in is a URL
 bool isURL(const string& word) {
-    regex url_regex(R"((http://|https://|www\.|[a-zA-Z0-9.-]+\.(com|org|net|edu|gov|io|co|lt)))");
+    regex url_regex(R"((https?://)?(www\.)?[a-zA-Z0-9\-]+\.(lt|com|org|net|edu|gov|io|co)(/\S*)?)");
     return regex_match(word, url_regex);
 }
