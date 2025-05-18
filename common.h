0c3fdc167b4823109ffc7303a74803e89d2c6037
Julius Truksinas <julius.truksinasinfo@gmail.com>
2025-05-18 17:20:05 +0300

    add common.h, open necessary file streams in main.cpp

---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <regex>
+#include <algorithm>
+#include <cctype>
+#include <set>
+
+using namespace std;
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+#include "functions.h"
+#include "common.h"
+
 int main() {
-    
+    ifstream input("text.txt");
+    ofstream wordOutput("output.txt");
+    ofstream urlOutput("url.txt");
+    ofstream xrefOutput("xref.txt");
+
+    if (!input.is_open()) {
+        cerr << "Failed to open text.txt\n";
+        return 1;
+    }
+
+    input.close();
+    wordOutput.close();
+    urlOutput.close();
+    xrefOutput.close();
+        
+    return 0;
 }
